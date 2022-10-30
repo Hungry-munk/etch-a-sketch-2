75a9145ff4fa0dedb2e7a752ceb60bdba624b3ee
Hungry_munk <abbaszadeharsam@gmail.com>
2022-10-31 09:47:12 +1100

    most of the CSS for the menu bar is complete it now looks good but the colour of things will likely be changed

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
-const gridContainer = document.getElementById('grid-container')
+const GRID_CONTAINER = document.getElementById('grid-container')
 
 let gridSize = 16
 
 
+//used for centering the canvas
+// const MAINTAG = document.querySelector('main')
+// const FOOTER = document.querySelector('footer')
+// const HEADER = document.querySelector('header')
+// const BODY = document.querySelector('body')
+
+// function resizeMainHeight() {
+//     MAINTAG.style.height = BODY.offsetHeight - (FOOTER.offsetHeight + HEADER.offsetHeight)
+// }
+
+
+
 function createGrid(gridSize) {
-    gridContainer.style.gridTemplateColumns= `repeat(${gridSize}, 1fr)`
-    gridContainer.style.gridTemplateRows= `repeat(${gridSize}, 1fr)`
+    GRID_CONTAINER.style.gridTemplateColumns= `repeat(${gridSize}, 1fr)`
+    GRID_CONTAINER.style.gridTemplateRows= `repeat(${gridSize}, 1fr)`
 
     for (i = 0; i < gridSize*gridSize; i++) {
         const cell = document.createElement('div')
         cell.classList.add('cell')
-        cell.setAttribute('draggable', false)
 
         cell.addEventListener('click', changeCellBackgroundColor)
         cell.addEventListener('mouseover',changeCellBackgroundColor)
-        gridContainer.appendChild(cell)
+        GRID_CONTAINER.appendChild(cell)
 
     }
 }
 
 //letting grid know when
 let mousedown = false
-gridContainer.onmousedown = ()=> mousedown = true
-gridContainer.onmouseup = ()=> mousedown = false
-gridContainer.onmouseleave = ()=> mousedown = false
+GRID_CONTAINER.onmousedown = ()=> mousedown = true
+GRID_CONTAINER.onmouseup = ()=> mousedown = false
+GRID_CONTAINER.onmouseleave = ()=> mousedown = false
 
 
 function changeCellBackgroundColor (event) {
